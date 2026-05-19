d238c5105a15bbdb12d5da8d3b4d58871daf9f72
Daniel Gonzalez <danielglezsolucionador@gmail.com>
2026-05-18 22:52:28 -0500

    fix task list count query without subquery

--- app/repositories/task_repository.py
+++ app/repositories/task_repository.py
@@ -84,9 +84,10 @@ class TaskRepository:
             if status:
                 base_q = base_q.where(Task.status == status)
 
-            count_result = await self.session.execute(
-                select(func.count()).select_from(base_q.subquery())
-            )
+            count_q = select(func.count(Task.id))
+            if status:
+                count_q = count_q.where(Task.status == status)
+            count_result = await self.session.execute(count_q)
             total = count_result.scalar_one()
 
             rows_result = await self.session.execute(
